f407e47abb737d349ad1ef7a1422d4bb52be5be7
joshua530 <joshuaomari5@gmail.com>
2022-11-23 22:17:34 +0300

    implement middleware

---
+++ backend/middleware/auth-middleware.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const { secretKey } = require('../config/secret-key');
+
+const authenticate = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    next();
+    return;
+  }
+
+  const token = req.body.token;
+  if (!token) {
+    res.status(401).json({ error: 'token required for authentication' });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const userSlug = decoded['user_slug'];
+    if (req.params.slug) {
+      // request has user slug in url
+      const requestSlug = req.params.slug;
+      if (userSlug !== requestSlug) {
+        res.status(403).json({ error: 'invalid token' });
+        return;
+      }
+    } else {
+      // request uses user slug in token for interacting with resources,
+      // extract it so that it can be used wherever it is required
+      req.userSlug = userSlug;
+    }
+  } catch (err) {
+    res.status(401).json({ error: 'invalid token' });
+    return;
+  }
+  next();
+};
+
+module.exports = authenticate;
---
+++ backend/middleware/headers-middleware.js
@@ -0,0 +1,8 @@
+const headerSetter = (req, res, next) => {
+  res.setHeader('Access-control-allow-origin', '*');
+  res.setHeader('Access-control-allow-headers', 'content-type');
+  res.setHeader('Access-control-allow-methods', 'PUT, POST, GET, DELETE');
+  next();
+};
+
+module.exports = headerSetter;
